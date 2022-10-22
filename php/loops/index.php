5fc35e982db1c6fe62b6da707c09fb07cbd70552
MichalRedm <michal.redm@gmail.com>
2022-10-22 14:50:16 +0200

    PHP loops

--- php/loops/index.php
+++ php/loops/index.php
@@ -8,3 +8,16 @@
 // If the number is a multiple of 4, write Fire instead of the number
 // If the number is a multiple of 7, write Boom instead of the number
 // If the number is a multiple of 10, repeat it 10 times in the same line in red
+?>
+
+<?php for ($i = 100; $i >= 0; $i--): ?>
+    <?php if ($i % 4 == 0): ?>
+        <p>Fire</p>
+    <?php elseif ($i % 7 == 0): ?>
+        <p>Boom</p>
+    <?php elseif ($i % 10 == 0): ?>
+        <p style="color: red"><?=implode(" ", array_fill(0, 10, $i))?></p>
+    <?php else: ?>
+        <p><?=$i?></p>
+    <?php endif; ?>
+<?php endfor; ?>
