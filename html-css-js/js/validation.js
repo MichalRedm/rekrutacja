65d6c6b0586cadff1052b5f0c09fbbffdfd47727
MichalRedm <michal.redm@gmail.com>
2022-10-23 22:11:50 +0200

    Update validation.js

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -1,15 +1,3 @@
-/*
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    AKAI Frontend Task - Javascript
-
-    W tym zadaniu postaraj się zaimplementować metody, które sprawdzą, czy dane wprowadzone
-    do formularza są poprawne. Przykładowo: czy imię i nazwisko zostało wprowadzone.
-    Możesz rozwinąć walidację danych tak bardzo, jak tylko zapragniesz.
-
-    Powodzenia!
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*/
-
 const form = document.getElementsByTagName("form")[0];
 
 document.querySelector("input[type=submit]").addEventListener("click", (event) => {
@@ -22,22 +10,3 @@ document.querySelector("input[type=submit]").addEventListener("click", (event) =
     invalids.forEach((elem) => elem.classList.add("invalid"));
     setTimeout(() => invalids.forEach((elem) => elem.classList.remove("invalid")), 400);
 });
-
-const
-    fnameInput = document.getElementById("firstName"),
-    lnameInput = document.getElementById("lastName"),
-    emailInput = document.getElementById("email");
-
-/*fnameInput.addEventListener("keyup", (event) => {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        lnameInput.focus();
-    }
-});
-
-lnameInput.addEventListener("keyup", (event) => {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        emailInput.focus();
-    }
-});*/
