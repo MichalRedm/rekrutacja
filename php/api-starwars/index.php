1c974d5503c6f246ebf95aa2a6b7cb201d0c69a0
MichalRedm <michal.redm@gmail.com>
2022-10-23 14:33:34 +0200

    Star Wars API improved

--- php/api-starwars/index.php
+++ php/api-starwars/index.php
@@ -36,10 +36,11 @@ $starships = json_decode($client->get("https://swapi.dev/api/starships")->getBod
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API Star Wars</title>
+    <title>Star Wars API</title>
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
+    <h1>Star Wars API - Starships</h1>
     <div id="starshipsContainer">
         <?php foreach ($starships as $starship): ?>
             <article class="starship">
@@ -52,6 +53,46 @@ $starships = json_decode($client->get("https://swapi.dev/api/starships")->getBod
                     <tr>
                         <td>Manufacturer</td>
                         <td><?=$starship->manufacturer?></td>
+                    </tr>
+                    <tr>
+                        <td>Cost in credits</td>
+                        <td><?=$starship->cost_in_credits?></td>
+                    </tr>
+                    <tr>
+                        <td>Length</td>
+                        <td><?=$starship->length?></td>
+                    </tr>
+                    <tr>
+                        <td>Max atm. speed</td>
+                        <td><?=$starship->max_atmosphering_speed?></td>
+                    </tr>
+                    <tr>
+                        <td>Crew</td>
+                        <td><?=$starship->crew?></td>
+                    </tr>
+                    <tr>
+                        <td>Passengers</td>
+                        <td><?=$starship->passengers?></td>
+                    </tr>
+                    <tr>
+                        <td>Cargo capacity</td>
+                        <td><?=$starship->cargo_capacity?></td>
+                    </tr>
+                    <tr>
+                        <td>Consumables</td>
+                        <td><?=$starship->consumables?></td>
+                    </tr>
+                    <tr>
+                        <td>Hyperdrive rating</td>
+                        <td><?=$starship->hyperdrive_rating?></td>
+                    </tr>
+                    <tr>
+                        <td>MGLT</td>
+                        <td><?=$starship->MGLT?></td>
+                    </tr>
+                    <tr>
+                        <td>Class</td>
+                        <td><?=$starship->starship_class?></td>
                     </tr>
                     <tr>
                         <td>Pilots</td>
